efc21645c96db158be0b21846491498dd5c9c173
Semyon Yakimov <sdyakimov@gmail.com>
2025-11-18 17:54:53 +0300

    fix: fix reverse order of tables after file load
    https://github.com/sportorg/pysport/commit/abdf288cd5385eb93985df37eceb9af838549831

--- sportorg/models/memory.py
+++ sportorg/models/memory.py
@@ -1515,7 +1515,7 @@ class Race(Model):
         obj = self.support_obj[dict_obj['object']]()
         obj.id = uuid.UUID(dict_obj['id'])
         self.update_obj(obj, dict_obj)
-        self.list_obj[dict_obj['object']].insert(0, obj)
+        self.list_obj[dict_obj['object']].append(obj)
 
     def get_type(self, group: Group):
         if group.get_type():
